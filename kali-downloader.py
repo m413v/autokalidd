3707f78abdb3e8d964d52b82e9bb5fa8fdc51464
m413v <m413v@protonmail.com>
2021-06-25 11:57:35 +0200

    downloading isos using threads

--- kali-downloader.py
+++ kali-downloader.py
@@ -3,16 +3,18 @@
 from bs4 import BeautifulSoup
 import requests
 import threading
-
+from pathlib import Path
 base_url = "https://cdimage.kali.org/current/"
 
 page = requests.get(base_url)
 kali = BeautifulSoup(page.content, "html.parser")
 
-def download_iso(url, filename):
+download_threads = list()
+
+def download(url, filename):
+	print("Downloading %s"%url)
 	response = requests.get(url, stream = True)
 	text_file = open(filename,"wb")
-
 	for chunk in response.iter_content(chunk_size=1024):
 		text_file.write(chunk)
 	text_file.close()
@@ -20,9 +22,18 @@ def download_iso(url, filename):
 
 
 for a in kali.find_all('a', href=True):
+	if "SHA256SUMS" in a['href']:
+		filename = a['href']
+		download(base_url+filename, filename)
 	if "amd64" in a['href']:
 		if "installer" in a['href'] or "live" in a['href']:
 			image = a['href']
-			print("Found the URL: %s, downloading... "%image)
-			download_iso(base_url+image, image)
-
+			iso = Path("./"+image)
+			if not iso.is_file():
+    			# file exists
+				print("Found the URL: %s, added to thread list. "%image)
+				x = threading.Thread(target=download, args=(base_url+image, image,))
+				download_threads.append(x)
+				x.start()
+			else:
+				print("%s exists in current dir, skipping."%image)
